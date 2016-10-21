51a9503867cdc7d0512025f8bb62eb090fec24a7
James Estes <james.estes@cloudability.com>
2016-10-20 20:52:54 -0600

    set interval for game loop

--- tide_game.js
+++ tide_game.js
@@ -2,13 +2,22 @@ function onMouseMove (e) {
     var x = e.clientX;
     findPaddle().style.left = (x-75) + 'px';
 };
+
 function findPaddle(){
 	return document.getElementById('paddle')
 }
+
+function gameLoop(){
+	console.log('in game loop')
+	// update ball position
+	// collision detection (walls & paddle)
+	// if game over, cancel the gameLoopInterval with clearInterval(gameLoopInterval)
+}
+var gameLoopInterval = setInterval(gameLoop, 1000)
 /*
- - create paddle
- - move paddle
- - create ball
+ * create paddle
+ * move paddle
+ * create ball
  - move ball
  - bounce ball off screen edge
  - bounce ball off paddle
