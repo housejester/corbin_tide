c6d707fef5f0b54fc9647870dc2f27a2a8476e51
James Estes <james.estes@gmail.com>
2016-10-21 19:55:06 -0600

    got ball moving

--- tide_game.js
+++ tide_game.js
@@ -4,21 +4,32 @@ function onMouseMove (e) {
 };
 
 function findPaddle(){
-	return document.getElementById('paddle')
+	return document.getElementById('paddle');
 }
 
 function gameLoop(){
-	console.log('in game loop')
-	// update ball position
+	moveBall();
 	// collision detection (walls & paddle)
 	// if game over, cancel the gameLoopInterval with clearInterval(gameLoopInterval)
 }
-var gameLoopInterval = setInterval(gameLoop, 1000)
+var ballSpeed = 10;
+function findBall(){
+	return document.getElementById('ball');
+}
+function moveBall(){
+	var ball = findBall();
+	var left = parseInt(ball.style.left) || 0;
+	var top = parseInt(ball.style.top) || 0;
+	ball.style.left = left + ballSpeed + 'px';
+	ball.style.top = top + ballSpeed + 'px';
+}
+var gameLoopInterval = setInterval(gameLoop, 1000/60)
+
 /*
  * create paddle
  * move paddle
  * create ball
- - move ball
+ * move ball
  - bounce ball off screen edge
  - bounce ball off paddle
  - count score
