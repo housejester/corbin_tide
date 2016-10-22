fb790d2ebb31e5bac52c81586b6272bf6f5480b1
James Estes <james.estes@gmail.com>
2016-10-21 20:32:10 -0600

    bouce ball off edges

--- tide_game.js
+++ tide_game.js
@@ -1,3 +1,5 @@
+var ballXSpeed = 10;
+var ballYSpeed = 10;
 function onMouseMove (e) {
     var x = e.clientX;
     findPaddle().style.left = (x-75) + 'px';
@@ -9,10 +11,29 @@ function findPaddle(){
 
 function gameLoop(){
 	moveBall();
+	//right edge
+	if(parseInt(ball.style.left) + 20 >= document.body.clientWidth){
+		ball.style.left = (document.body.clientWidth - 21) + 'px'
+		ballXSpeed = -ballXSpeed
+	}
+	//top
+	else if(parseInt(ball.style.top) <= 0){
+		ball.style.top = 1 + 'px'
+		ballYSpeed = -ballYSpeed
+	}
+	//left edge
+	else if(parseInt(ball.style.left) <= 0){
+		ball.style.left = 1 + 'px'
+		ballXSpeed = -ballXSpeed
+	}
+	else if(parseInt(ball.style.top) + 20 >= document.body.clientHeight){
+		ball.style.top = ball.style.top = (document.body.clientHeight - 21) + 'px'
+		ballYSpeed = -ballYSpeed
+	}
 	// collision detection (walls & paddle)
 	// if game over, cancel the gameLoopInterval with clearInterval(gameLoopInterval)
 }
-var ballSpeed = 10;
+
 function findBall(){
 	return document.getElementById('ball');
 }
@@ -20,8 +41,8 @@ function moveBall(){
 	var ball = findBall();
 	var left = parseInt(ball.style.left) || 0;
 	var top = parseInt(ball.style.top) || 0;
-	ball.style.left = left + ballSpeed + 'px';
-	ball.style.top = top + ballSpeed + 'px';
+	ball.style.left = left + ballXSpeed + 'px';
+	ball.style.top = top + ballYSpeed + 'px';
 }
 var gameLoopInterval = setInterval(gameLoop, 1000/60)
 
